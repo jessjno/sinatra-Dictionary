d0e81a15e27dea75666ebc8aa0cb6229b941d062
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-08-24 04:13:14 +0000

    fixed spanish API

--- app.rb
+++ app.rb
@@ -45,19 +45,45 @@ get("/translate") do
   erb(:translation_form)
 end
 
+get("/translate") do
+  erb(:translation_form)
+end
+
 post("/get_translation") do
   @translate_word = params.fetch("translate_user_word")
   translate_key = ENV.fetch("TRANSLATE_KEY")
-  ref = "spanish"
   
-  url = URI("https://dictionaryapi.com/api/v3/references/spanish/json/#{ref}/json/#{@translate_word}?key=#{translate_key}")
-
+  url = URI("https://dictionaryapi.com/api/v3/references/spanish/json/#{@translate_word}?key=#{translate_key}")
 
   response = Net::HTTP.get(url)
 
   @response_string = response.to_s
 
-  #@translation = JSON.parse(response_string)
+  # Uncomment the line below to parse the response if it's in JSON format
+  @translations = JSON.parse(@response_string)
 
+  if @translations.any?
+    @translation = @translations[0]['shortdef'][0]
+    puts @translation
+  else
+    puts "No translation found."
+  end 
   erb(:translation_results)
 end
+
+# post("/get_translation") do
+#   @translate_word = params.fetch("translate_user_word")
+#   translate_key = ENV.fetch("TRANSLATE_KEY")
+#   ref = "spanish"
+  
+#   url = URI("https://dictionaryapi.com/api/v3/references/spanish/json/#{ref}/json/#{@translate_word}?key=#{translate_key}")
+
+
+#   response = Net::HTTP.get(url)
+
+#   @response_string = response.to_s
+
+#   #@translation = JSON.parse(response_string)
+
+#   erb(:translation_results)
+# end
